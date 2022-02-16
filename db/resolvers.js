9b634e9b880ccd08ed73727a09c617e0e0883195
Baruch Guerra <dolce_dmn@hotmail.com>
2022-02-15 19:26:41 -0600

    Added the hashing to passwords, modificated schema file, improved mutation function

--- db/resolvers.js
+++ db/resolvers.js
@@ -4,19 +4,20 @@ const cursos = [
     technology: 'JavaScript ES6',
   },
   {
-    title: 'React – La Guía Completa: Hooks Context Redux MERN +15 Apps',
+    title: 'React - La Guía Completa: Hooks Context Redux MERN +15 Apps',
     technology: 'React',
   },
   {
-    title: 'Node.js – Bootcamp Desarrollo Web inc. MVC y REST API’s',
+    title: 'Node.js - Bootcamp Desarrollo Web inc. MVC y REST APIs',
     technology: 'Node.js'
   },
   {
-    title: 'ReactJS Avanzado – FullStack React GraphQL y Apollo',
+    title: 'ReactJS Avanzado - FullStack React GraphQL y Apollo',
     technology: 'React'
   }
 ];
-
+const bcryptjs = require('bcryptjs');
+const User = require('../models/User');
 // Resolvers
 const resolvers = {
   Query: {
@@ -27,9 +28,27 @@ const resolvers = {
     // getTechnology: () => cursos
   },
   Mutation: {
-    newUser: (_, { input }) => {
-      console.log(input);
-      return 'CREANDO...';
+    newUser: async (_, { input }) => {
+      const { email, password } = input;
+      // Revisar si usuario está registrado 
+      const userExists = await User.findOne({ email });
+      console.log(userExists);
+      if (userExists) {
+        throw new Error('User is already registered.s');
+      }
+      // Hashear su password 
+      const salt = await bcryptjs.genSalt(10);
+      input.password = await bcryptjs.hash(password, salt);
+
+      try {
+        // Guardarlo en la BBDD
+        const user = new User(input);
+        user.save(); // Se almacena en la BBDD
+
+        return user;
+      } catch (error) {
+        console.log(error);
+      }
     }
   }
 }
