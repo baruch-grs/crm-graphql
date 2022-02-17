bd69169ff69b595189cf86ec965f2d59a51a3499
Baruch Guerra <dolce_dmn@hotmail.com>
2022-02-17 14:15:15 -0600

    Added all CRUD functionalities, added new model Product, added new model Clients, modified schema to products and clients, added resolver methods to resolve schema methods

--- db/resolvers.js
+++ db/resolvers.js
@@ -21,6 +21,9 @@ require('dotenv').config({ path: 'variables.env' });
 const jwt = require('jsonwebtoken');
 const bcryptjs = require('bcryptjs');
 const User = require('../models/User');
+const Product = require('../models/Product');
+const Client = require('../models/Clients');
+
 
 const createToken = (user, passphrase, expiresIn) => {
   console.log(user);
@@ -30,11 +33,29 @@ const createToken = (user, passphrase, expiresIn) => {
 // Resolvers
 const resolvers = {
   Query: {
-    getCourses: (_, { input }, context) => {
-      const result = cursos.filter((curso) => curso.technology === input.technology);
-      return result;
+    getUser: async (_, { token }) => {
+      const userId = await jwt.verify(token, process.env.PASSPHRASE);
+      return userId;
     },
-    // getTechnology: () => cursos
+    getProducts: async () => {
+      try {
+        const products = await Product.find({});
+        return products;
+      } catch (error) {
+        console.log('CANNOT GET PRODUCTS', error);
+      }
+    },
+
+    getProductByID: async (_, { id }) => {
+      // Revisar si existe producto
+      const product = await Product.findById(id);
+
+      if (!product) {
+        throw new Error('Product not found');
+      }
+
+      return product;
+    }
   },
   Mutation: {
     newUser: async (_, { input }) => {
@@ -75,10 +96,70 @@ const resolvers = {
       }
 
       return {
+        // Crear token
         token: createToken(userExists, process.env.PASSPHRASE, '24h')
       }
 
-      // Crear token
+    },
+
+    newProduct: async (_, { input }) => {
+      try {
+        const product = new Product(input);
+
+        // Almacenar en BBDD
+        const result = await product.save();
+        return result;
+      } catch (error) {
+        console.log('ERROR. CANNOT CREATE PRODUCT', error);
+      }
+    },
+
+    updateProduct: async (_, { id, input }) => {
+      // Revisar si existe producto
+      let product = await Product.findById(id);
+
+      if (!product) {
+        throw new Error('Product not found');
+      }
+
+      // Guardarlo en la BBDD
+      product = await Product.findOneAndUpdate({ _id: id, }, input, { new: true });
+      return product;
+    },
+    deleteProduct: async (_, { id }) => {
+      // Revisar si existe producto
+      let product = await Product.findById(id);
+      if (!product) {
+        throw new Error('Product not found');
+      }
+
+      // Eliminar producto
+      await Product.findOneAndDelete({ _id: id });
+
+      return 'Producto eliminado';
+    },
+
+    newClient: async (_, { input }) => {
+      const { email } = input;
+      // Verificar si el cliente está registrado
+      console.log(input);
+      const client = Client.findOne({ email });
+
+      if (client) {
+        throw new Error('Client already registered');
+      }
+      const newClient = new Client(input);
+
+      // Asignar vendedor
+      newClient.seller = "620e8ffdb6cb9b9b6dda706d";
+      // Guardar en BBDD
+      try {
+        const result = await newClient.save();
+        return result
+
+      } catch (error) {
+        console.log('ERROR, CLIENT NOT REGISTERED', error);
+      }
     }
   }
 }
--- db/schema.js
+++ db/schema.js
@@ -13,6 +13,24 @@ const typeDefs = gql`
     token: String
   }
 
+  type Product {
+    id: ID
+    name: String
+    availability: Int
+    price: Float
+    created: String
+  }
+
+  type Client {
+    id: ID
+    name: String
+    lastName: String
+    company: String
+    email: String
+    phone: String
+    seller: ID
+  }
+
   input AuthenticateInput {
     email: String
     password: String!
@@ -25,13 +43,42 @@ const typeDefs = gql`
     password: String!
   }
 
+  input ProductInput {
+    name: String!
+    availability: Int!
+    price: Float!
+  }
+
+  input ClientInput {
+    name: String!
+    lastName: String!
+    company: String!
+    email: String!
+    phone: String
+  }
+
   type Query {
-    getCourses: String
+    # Users
+    getUser(token: String!): User
+
+    # Products
+    getProducts: [Product]
+    getProductByID(id: ID!): Product 
   }
 
   type Mutation {
+    # Users
     newUser(input: UserInput): User
     authenticateUser(input: AuthenticateInput): Token
+
+    # Products
+    newProduct(input: ProductInput): Product
+    updateProduct(id: ID!, input: ProductInput): Product
+    deleteProduct(id: ID!): String
+    
+    # Clients
+    newClient(input: ClientInput): Client
+
   }
 `;
 
